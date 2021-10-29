d6d06eaa06dd4037c27cdc07004a8bde3141f9bc
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-29 11:46:13 -0300

    Modelar Controller  de Lead

--- src/main/java/br/com/zup/exercicio2/controller/LeadController.java
+++ src/main/java/br/com/zup/exercicio2/controller/LeadController.java
@@ -1,4 +1,15 @@
 package br.com.zup.exercicio2.controller;
 
+import br.com.zup.exercicio2.service.LeadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/leads")
 public class LeadController {
+
+    @Autowired
+    private LeadService leadService;
+
 }
