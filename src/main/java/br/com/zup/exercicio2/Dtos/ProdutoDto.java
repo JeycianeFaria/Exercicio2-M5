df024ef8e8ed153958fafd3583a44ca7178b248d
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-29 11:33:09 -0300

    Modelar DTO de produtos

--- src/main/java/br/com/zup/exercicio2/Dtos/ProdutoDto.java
+++ src/main/java/br/com/zup/exercicio2/Dtos/ProdutoDto.java
@@ -1,4 +1,34 @@
 package br.com.zup.exercicio2.Dtos;
 
 public class ProdutoDto {
+
+    private String nomeDoProduto;
+    private double valorDoProduto;
+
+
+    public ProdutoDto() {
+    }
+
+    public ProdutoDto(String nomeDoProduto, double valorDoProduto) {
+        this.nomeDoProduto = nomeDoProduto;
+        this.valorDoProduto = valorDoProduto;
+    }
+
+
+    public String getNomeDoProduto() {
+        return nomeDoProduto;
+    }
+
+    public void setNomeDoProduto(String nomeDoProduto) {
+        this.nomeDoProduto = nomeDoProduto;
+    }
+
+    public double getValorDoProduto() {
+        return valorDoProduto;
+    }
+
+    public void setValorDoProduto(double valorDoProduto) {
+        this.valorDoProduto = valorDoProduto;
+    }
+
 }
