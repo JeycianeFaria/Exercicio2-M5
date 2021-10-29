ba5a86060d6b7a032119c115a3cf3074bc1c867f
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-29 11:42:20 -0300

    Modelar  Service de Lead

--- src/main/java/br/com/zup/exercicio2/service/LeadService.java
+++ src/main/java/br/com/zup/exercicio2/service/LeadService.java
@@ -1,7 +1,13 @@
 package br.com.zup.exercicio2.service;
 
+import br.com.zup.exercicio2.dtos.LeadDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LeadService {
+
+    private List<LeadDto> leads;
+
 }
