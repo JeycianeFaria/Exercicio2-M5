87edc2382f415e278fea12ca71be939b40c1ba79
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-29 11:36:02 -0300

    Modelar DTO de lead

--- src/main/java/br/com/zup/exercicio2/Dtos/LeadDto.java
+++ src/main/java/br/com/zup/exercicio2/Dtos/LeadDto.java
@@ -1,4 +1,56 @@
 package br.com.zup.exercicio2.Dtos;
 
+import java.util.List;
+
 public class LeadDto {
+
+    private String email;
+    private String nome;
+    private String telefone;
+    private List<ProdutoDto> listaProdutos;
+
+
+    public LeadDto() {
+    }
+
+    public LeadDto(String email, String nome, String telefone, List<ProdutoDto> listaProdutos) {
+        this.email = email;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.listaProdutos = listaProdutos;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<ProdutoDto> getListaProdutos() {
+        return listaProdutos;
+    }
+
+    public void setListaProdutos(List<ProdutoDto> listaProdutos) {
+        this.listaProdutos = listaProdutos;
+    }
+
 }
